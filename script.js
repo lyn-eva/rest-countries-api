959e41f4233818608a3722418745c677176864fe
Zeiyar Lynn <zeiyarlynn@gmail.com>
2021-12-03 15:24:43 +0630

    added filter by region option

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 const filter = document.getElementById("filter");
 const options = document.querySelector(".options");
+const loader = document.querySelector('.loader');
 
-filter.onclick = (e) => {
-   options.classList.toggle("options-visible");
-};
-options.onclick = (e) => {
-   if (e.target.tagName == "OPTION") {
-      filter.innerText = e.target.innerText;
-      options.classList.remove("options-visible");
-   }
-};
 
 document.addEventListener("DOMContentLoaded", (e) => {
+   let data;
+   
    fetch("https://restcountries.com/v3.1/all", { method: "get" })
-      .then((response) => {
-         console.log(response);
-         return response.json();
-      })
-      .then((response) => {
-         console.log(response);
-         const mainContent = generateCards(response);
-         document.querySelector(".main-hdr").after(mainContent);
-      })
-      .catch((error) => {
-         console.log(error);
-      });
+   .then((response) => {
+      // console.log(response);
+      return response.json();
+   })
+   .then((response) => {
+      data = response;
+      const mainContent = generateCards(response);
+      loader.style.display = "none";
+      document.querySelector(".main-hdr").after(mainContent);
+   })
+   .catch((error) => {
+      console.log(error);
+            
+      document.querySelector('.loader').after("Please Check Your Internet Connection");
    });
+   
+   filter.onclick = (e) => {
+      options.classList.toggle("options-visible");
+   };
+   //filter by region
+   options.onclick = (e) => {
+      if (e.target.tagName == "OPTION") {
+         filter.innerText = e.target.innerText;
+         options.classList.remove("options-visible");
+   
+         filterByRegion(data, e.target.innerText);
+      }
+   };
+});
    
 function generateCards(cards) {
    const mainContent = document.createElement('div');
@@ -53,9 +63,21 @@ function createCard(name, pop, region, capital, flagURL) {
    Name.innerHTML = name;
 
    flag.innerHTML = `<img src=${flagURL} alt=${name}>`;
-   general.innerHTML = `<p>Population: <span>${pop}</span></p><p>Region: <span>${region}</span></p><p>Capital: <span>${capital}</span></p>`;
+   general.innerHTML = `<p>Population: <span>${pop.toLocaleString()}</span></p><p>Region: <span>${region}</span></p><p>Capital: <span>${capital}</span></p>`;
 
    details.append(Name, general);
    card.append(flag, details);
    return card;
+}
+
+function filterByRegion(cards, filterName) {
+   const childs = document.querySelectorAll('.card');
+   childs.forEach(child => {
+      child.style.display = "block";
+   })
+   cards.forEach( (card, i) => {
+      const {region} = card;
+      if (region !== filterName)
+         childs[i].style.display = "none";
+   })
 }
