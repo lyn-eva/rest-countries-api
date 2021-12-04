793b7b8f5d27d3dd596ea8404124b6a79297c230
Zeiyar Lynn <zeiyarlynn@gmail.com>
2021-12-05 00:30:10 +0630

    added more-details section

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ const options = document.querySelector(".options");
 const loader = document.querySelector('.loader');
 const mode = document.getElementById('mode');
 const input = document.getElementById('input');
+const main = document.querySelector(".main-hdr");
 
 document.addEventListener("DOMContentLoaded", () => {
    let data;   
    fetch("https://restcountries.com/v3.1/all", { method: "get" })
    .then((response) => {
-      // console.log(response);
+      // console.log(response.json());
       return response.json();
    })
    .then((response) => {
+      console.log(response);
       data = response;
       const mainContent = generateCards(response);
       loader.style.display = "none";
-      document.querySelector(".main-hdr").after(mainContent);
+      main.after(mainContent);
    })
    .catch((error) => {
       console.log(error);  
@@ -53,13 +55,23 @@ document.addEventListener("DOMContentLoaded", () => {
 });
    
 function generateCards(cards) {
+   const more = document.querySelector('.more-details');
    const mainContent = document.createElement('div');
    mainContent.setAttribute('class', "main-content");
 
-   cards.forEach(cd => {
+   cards.forEach((cd, i) => {
       const {name: { common }, population,region, capital, flags: { png },} = cd;
       const temp = createCard(common, population, region, capital, png);   
+      temp.setAttribute('id', i);
       mainContent.appendChild(temp);
+
+      temp.addEventListener('click', () => {
+         if (!more.firstElementChild.nextElementSibling) {
+            main.nextElementSibling.style.display = "none";
+            more.style.display = "block";
+            more.firstElementChild.after(moreDetailsPage(cards, i));
+         }
+      });
    });
 
    return mainContent;
@@ -118,3 +130,61 @@ function neutralize(cards) {
       card.style.display = "block";
    })
 }
+
+function moreDetailsPage(cards, id) {
+   const DetailsCtr = document.createElement('div');
+   DetailsCtr.setAttribute("class", 'details-ctr flex');
+   const flag = document.createElement('div');
+   flag.setAttribute("class", 'flag');
+   const h2 = document.createElement('h2');
+   h2.setAttribute("id", 'country-name');
+   const country = document.createElement('div');
+   country.setAttribute("class", 'country');
+   const countryDetails = document.createElement('div');
+   countryDetails.setAttribute("class", 'country-details flex');
+   const col1 = document.createElement('div');
+   col1.setAttribute("class", 'col-1');
+   const col2 = document.createElement('div');
+   col2.setAttribute("class", 'col-2');
+   const borders = document.createElement('div');
+   borders.setAttribute("class", 'borders');
+
+   const {name : {common, nativeName}, flags: {svg}, population, region, subregion, capital, tld, currencies, languages} = cards[id];
+   h2.innerHTML = common;
+   flag.innerHTML = `<img src=${svg} alt=${common}>`;
+   col1.innerHTML = `<p>Native Name: <span>${nativeName[Object.keys(nativeName)[0]].official}</span></p><p>Population: <span>${population}</span></p><p>Region: <span>${region}</span></p><p>Sub Region: <span>${subregion}</span></p><p>Capital: <span>${capital ? capital : "unknown"}</span></p>`;
+   col2.innerHTML = `<p>Top Level Domain: <span>${tld.join(", ")}</span></p><p>Currencies: <span>${currencies[Object.keys(currencies)].name}</span></p><p>Languages: <span>${Object.values(languages).join(", ")}</span></p>`;
+   borders.innerHTML = `<span>Border Countries: </span><button type="button"></button><button type="button"></button><button type="button"></button>`;
+
+   countryDetails.append(col1, col2);
+   country.append(h2, countryDetails, borders);
+   DetailsCtr.append(flag, country);
+
+   return DetailsCtr;
+}
+{/* <div class="details-ctr flex">
+<div class="flag"></div>
+<div class="country">
+   <h2>Belgium</h2>
+   <div class="country-details flex">
+      <div class="col-1">
+         <p>Native Name: <span>Belgie</span></p>
+         <p>Population: <span>Belgie</span></p>
+         <p>Region: <span>Belgie</span></p>
+         <p>Sub Region: <span>Belgie</span></p>
+         <p>Capital: <span>Belgie</span></p>
+      </div>
+      <div class="col-2">
+         <p>Top Level Domain: <span>Belgie</span></p>
+         <p>Currencies: <span>Belgie</span></p>
+         <p>Languages: <span>Dutch, French, German</span></p>
+      </div>
+   </div>
+   <div class="borders">
+      <span>Border Countries: </span>
+      <button type="button">France</button>
+      <button type="button">Germany</button>
+      <button type="button">Netherlands</button>
+   </div>
+</div>
+</div> */}
